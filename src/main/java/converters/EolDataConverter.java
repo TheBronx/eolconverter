2cdf54b1f48e3946dcbdf22a0ebb21c9199eaf04
slabajos <slabajos@idealista.com>
2017-10-28 20:20:14 +0200

    refactoring EolDataConverter a bit

--- src/main/java/converters/EolDataConverter.java
+++ src/main/java/converters/EolDataConverter.java
@@ -15,33 +15,37 @@ public class EolDataConverter {
     }
 
     /**
-     * There is no such thing as C# "out" in Java so instead I am returning outputData and length in an Object
+     * There is no such thing as C# "out" in Java so instead I am returning outputData and length in an Object (DataBytes)
      */
     public DataBytes convert(byte[] data, int dataLength) {
         Encoding encoding = encodingGuesser.guess(data, dataLength);
 
-        byte[] convertedData = convertData(encoding, data, dataLength);
+        byte[] convertedData = convertData(data, dataLength, encoding);
 
         return new DataBytes(convertedData, convertedData.length);
     }
 
-    private byte[] convertData(Encoding encoding, byte[] data, int dataLength) {
+    private byte[] convertData(byte[] data, int dataLength, Encoding encoding) {
         EolConverter converter = EolConverterFactory.getConverterFor(encoding);
         Parser parser = new Parser(data, dataLength, encoding);
 
         byte[] output = new byte[dataLength * 2];
         int outputLength = 0;
 
-        byte[] nextChar = parser.consumeNext();
-        while(nextChar != null) {
-            byte[] convertedChar = converter.convert(nextChar, eolConversion, parser);
-            System.arraycopy(convertedChar, 0, output, outputLength, convertedChar.length);
+        byte[] charToConvert = parser.consumeNext();
+        while(charToConvert != null) {
+            byte[] convertedChar = converter.convert(charToConvert, eolConversion, parser);
+            copyToOutput(output, outputLength, convertedChar);
             outputLength += convertedChar.length;
 
-            nextChar = parser.consumeNext();
+            charToConvert = parser.consumeNext();
         }
 
         return getDataBytes(output, outputLength);
+    }
+
+    private void copyToOutput(byte[] output, int outputLength, byte[] convertedChar) {
+        System.arraycopy(convertedChar, 0, output, outputLength, convertedChar.length);
     }
 
     private byte[] getDataBytes(byte[] output, int outputLength) {
